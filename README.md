bef25fef6889d721d6091d857f25ba6505fc6193
Karolina <kika.romanska@gmail.com>
2018-01-06 14:47:20 +0100

    Live version availble

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Flex-Panels-Image-Gallery
+Live version available at :  https://dryadalis.github.io/Flex-Panels-Image-Gallery/
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const panels = document.querySelectorAll('.panel');
+
 function toggleOpen() {
   console.log('Hello');
   this.classList.toggle('open');
